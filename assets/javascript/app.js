d70d69f3c66e8c8f24e3cea5acfe33b22274d84c
Grace <bermejograce@gmail.com>
2018-12-08 23:59:34 -0700

    update

---
+++ assets/javascript/app.js
@@ -0,0 +1,70 @@
+// set variables 
+
+var triviaQuestion = [{
+    question: "Which Corey was in Goonies and The Lost Boys?",
+    choices: ["Haim", "Feldman"],
+    answer: 1
+}, {
+    question:"Which is of these actors was not part of the 80's Brat Pack?",
+    choices: [""],
+    answer: 1
+
+}, {
+    question:"In what movie did was Molly Ringwald's character a rich spoiled brat?",
+    choices: ["Pretty in Pink", "The Breakfast Club"],
+    answer: 1
+
+}, {
+    question:"?",
+    choices: [""],
+    answer: 
+
+}, {
+    question:"I",
+    choices: [""],
+    answer: 
+
+ }, {
+    question:"?",
+    choices: [""],
+    answer: 
+
+}, {
+    question:"?",
+    choices: [""],
+    answer: 
+
+}, {
+    question:"?",
+    choices: ["W"],
+    answer: 
+
+}
+
+];
+
+var game
+var score
+var timer
+
+
+
+
+// $("#start").click(function(){
+//     $(this).hide();
+//     counter = setInterval(timer, 1000); 
+//     displayTrivia();
+//     }); 
+    
+    
+//     function timer(){
+//     count--;
+//     if (count <= 0) {
+//      clearInterval(counter);
+//      return;
+//     }
+
+
+// $("#start").html('Time left: ' + '00:' + count + 'secs');  
+//     }
+
